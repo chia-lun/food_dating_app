a98b915f5fa6cd28e79d5ccf083022b801248e1a
Hengrui Jia <73600480+MrHari2002@users.noreply.github.com>
2021-09-29 12:41:47 -0500

    Setup for messagePage

---
+++ lib/messagePage.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+/**
+ * Author: Hengrui Jia
+ * This is the message page
+ */
+
+class messagePage extends StatefulWidget {
+  @override
+  messagePageState createstate() => messagePageState();
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class messagePageState extends State<messagePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
