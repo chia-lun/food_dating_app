26f7d2d5c94a38cce1c5cf0a722e33ad30904faa
Hengrui Jia <73600480+MrHari2002@users.noreply.github.com>
2021-10-07 21:10:15 -0500

    updating ontap

--- lib/message_page.dart
+++ lib/message_page.dart
@@ -5,6 +5,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_dating_app/models/message_model.dart';
 
+import 'chat_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -30,41 +32,50 @@ class MessagePageState extends State<MessagePage> {
         itemCount: chats.length,
         itemBuilder: (BuildContext context, int index) {
           final Message chat = chats[index];
-          return Row(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 35.0,
-                backgroundImage: AssetImage(chat.sender.imageUrl),
-              ),
-              SizedBox(width: 10.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    chat.sender.name,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
+          return GestureDetector(
+              onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ChatScreen(
+                        user: chat.sender,
+                      ),
                     ),
                   ),
-                  SizedBox(height: 5.0),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.45,
-                    child: Text(
-                      chat.text,
-                      style: TextStyle(
-                        color: Colors.blueGrey,
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w600,
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 35.0,
+                    backgroundImage: AssetImage(chat.sender.imageUrl),
+                  ),
+                  SizedBox(width: 10.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        chat.sender.name,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
+                      SizedBox(height: 5.0),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        child: Text(
+                          chat.text,
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
-              ),
-            ],
-          );
+              ));
         });
   }
 
