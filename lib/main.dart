4c917c38b0693c1961418ec40ca138842290b071
nedmayo <emayo@macalester.edu>
2021-10-11 21:41:42 -0500

    push for jiaying to use

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:food_dating_app/routes.dart';
 
 void main() => runApp(MyApp());
---
+++ lib/views/accounts/sign_in_form.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_dating_app/views/accounts/sign_in_controller.dart';
+import 'package:food_dating_app/widgets/input_text.dart';
+
+class _SignInView extends StatefulWidget {
+  @override
+  _SignInViewState createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<_SignInView> {
+  SignInController signInController = new SignInController();
+  String _email = '';
+  String _password = '';
+
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 180.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('assets/images/icon.jpg')),
+              ),
+            ),
+            StreamBuilder(
+              stream: signInController.emailStream,
+              builder: (context, snapshot) => InputText(
+                  title: 'Email',
+                  controller: _textController,
+                  errorText: snapshot.hasError ? snapshot.error.toString() : '',
+                  icon: CupertinoIcons.envelope,
+                  onValueChange: (value) {
+                    _email = value;
+                  }),
+            ),
+            StreamBuilder(
+              stream: signInController.passwordStream,
+              builder: (context, snapshot) => InputText(
+                  title: 'Password',
+                  controller: _textController,
+                  errorText: snapshot.hasError ? snapshot.error.toString() : '',
+                  icon: CupertinoIcons.lock,
+                  onValueChange: (value) {
+                    _password = value;
+                  }),
+            ),
+            TextButton(
+              onPressed: () {
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.orange, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            const Text('New User? Create Account')
+          ],
+        ),
+      ),
+    );
+  }
+}
