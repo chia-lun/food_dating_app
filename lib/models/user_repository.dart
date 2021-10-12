2d56adc99aa52104b97f01bf1a5be4a72783b4ce
James Yang <cyang4@macalester.edu>
2021-10-11 21:14:55 -0500

    added sawyer to the user database

--- lib/models/user_repository.dart
+++ lib/models/user_repository.dart
@@ -6,6 +6,7 @@ class UserRepository {
     User(id: 002, name: 'Ned Mayo', imageUrl: 'assets/images/ned.jpg'),
     User(id: 003, name: 'Hari Jia', imageUrl: 'assets/images/hari.jpg'),
     User(id: 004, name: 'Jimmy Yang', imageUrl: 'assets/images/james.jpg'),
+    User(id: 005, name: 'Sawyer Neske', imageUrl: 'assets/images/sawyer.jpg'),
   ];
 
   List<User> loadUsers() {
