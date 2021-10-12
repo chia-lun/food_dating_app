0f9798b15b0abd857f7f0f16f8492f6ee7a45ac2
JiayingWu-work <jwu4@macalester.edu>
2021-10-11 21:25:03 -0500

    test

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
 //import 'package:card_settings/card_settings.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,6 +15,15 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(
+            "Profile Setting",
+            textAlign: TextAlign.justify,
+          ),
+        ),
+        child: CupertinoTextField(
+          placeholder: "HEllo",
+        ));
   }
 }
