f7e89d87ac61bb67070d134aef5fe42d9a2836a3
James Yang <cyang4@macalester.edu>
2021-10-19 14:34:24 -0500

    added "login_signup_page" route & renamed "app" route to "swipe_message_profile" route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,20 +1,29 @@
 // import 'package:food_dating_app/views/chat_screen.dart';
-import 'package:food_dating_app/app.dart';
+// ignore: avoid_web_libraries_in_flutter
+//import 'dart:js';
+
+import 'package:food_dating_app/swipe_message_profile.dart';
 import 'package:food_dating_app/views/accounts/sign_in_controller.dart';
 import 'package:food_dating_app/views/home_page.dart';
-import 'package:food_dating_app/views/login_page.dart';
+import 'package:food_dating_app/views/login_signup_page.dart';
 import 'package:food_dating_app/views/message_page.dart';
 import 'package:food_dating_app/views/profile_page.dart';
 import 'package:food_dating_app/views/accounts/sign_in_form.dart';
 
-const initialRoute = "app";
+const initialRoute = "login_signup_page";
 
 var routes = {
-  'home_page': (context) => HomePage(),
-  // 'chat_page': (context) => ChatPage(),
+  //'chat_page': (context) => ChatPage(),
   //'login_page': (context) => LoginPage(),
-  'message_page': (context) => MessagePage(),
-  'profile_page': (context) => ProfilePage(),
-  'sign_in': (context) => SignInView(),
-  'app': (context) => App()
+  //'sign_in': (context) => SignInView(),
+
+  //swipe_message_profile is the page that allows users to navigate between 
+  //swipe page (home page), message page (where they can see people
+  //who messaged) and profile page (where they can modify their settings)
+  'swipe_message_profile': (context) => const SwipeMessageProfile(),
+
+  //login_signup_page is the page that users use to sign up or log in, after 
+  //either sign up bottom or log in bottom is on press, they will be navigated
+  //to swipe_message_profile page 
+  'login_signup_page': (context) => LoginSignupPage()
 };
