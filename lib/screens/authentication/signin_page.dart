8f08fd168e776beedae1b7dc8de93ec0fedb1c16
JiayingWu-work <70664833+JiayingWu-work@users.noreply.github.com>
2022-03-01 01:30:22 -0600

    removed unnecessary containers based on new Dart version

--- lib/screens/authentication/signin_page.dart
+++ lib/screens/authentication/signin_page.dart
@@ -10,6 +10,8 @@ import 'package:provider/provider.dart';
 class SignInPage extends StatefulWidget {
   static String tag = 'signin-page';
 
+  const SignInPage({Key? key}) : super(key: key);
+
   @override
   State<SignInPage> createState() => _SignInPageWidgetState();
 }
@@ -20,7 +22,7 @@ class _SignInPageWidgetState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
-    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+    //AuthProvider authProvider = Provider.of<AuthProvider>(context);
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -35,7 +37,7 @@ class _SignInPageWidgetState extends State<SignInPage> {
         ),
       ),
       Container(
-        color: Color.fromRGBO(0, 0, 0, 0.55),
+        color: const Color.fromRGBO(0, 0, 0, 0.55),
       ),
       Container(
         margin: EdgeInsets.only(
@@ -78,30 +80,29 @@ class _SignInPageWidgetState extends State<SignInPage> {
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-                child: MaterialButton(
+            MaterialButton(
               onPressed: () => {},
-              color: Color.fromRGBO(255, 255, 255, 0.35),
+              color: const Color.fromRGBO(255, 255, 255, 0.35),
               minWidth: size.width * 1,
               height: size.width * 0.1,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20),
               ),
               child: TextField(
-                style: TextStyle(color: Colors.white),
+                style: const TextStyle(color: Colors.white),
                 keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(
                   hintText: 'Email',
                   border: InputBorder.none,
                 ),
-                cursorColor: Color.fromRGBO(255, 255, 255, 0.75),
+                cursorColor: const Color.fromRGBO(255, 255, 255, 0.75),
                 onChanged: (value) => {
                   setState(() {
                     _email = value.trim();
                   })
                 },
               ),
-            )),
+            ),
           ],
         ),
       ),
@@ -115,10 +116,9 @@ class _SignInPageWidgetState extends State<SignInPage> {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                  child: MaterialButton(
+              MaterialButton(
                 onPressed: () => {},
-                color: Color.fromRGBO(255, 255, 255, 0.35),
+                color: const Color.fromRGBO(255, 255, 255, 0.35),
                 minWidth: size.width * 1,
                 height: size.width * 0.1,
                 shape: RoundedRectangleBorder(
@@ -126,20 +126,20 @@ class _SignInPageWidgetState extends State<SignInPage> {
                 ),
                 child: TextField(
                   obscureText: true,
-                  style: TextStyle(color: Colors.white),
+                  style: const TextStyle(color: Colors.white),
                   keyboardType: TextInputType.emailAddress,
                   decoration: const InputDecoration(
                     hintText: 'Password',
                     border: InputBorder.none,
                   ),
-                  cursorColor: Color.fromRGBO(255, 255, 255, 0.75),
+                  cursorColor: const Color.fromRGBO(255, 255, 255, 0.75),
                   onChanged: (value) => {
                     setState(() {
                       _password = value.trim();
                     })
                   },
                 ),
-              )),
+              ),
             ],
           )),
       Container(
@@ -152,31 +152,29 @@ class _SignInPageWidgetState extends State<SignInPage> {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                child: MaterialButton(
-                    minWidth: size.width * 1,
-                    height: size.width * 0.12,
-                    color: Color.fromRGBO(199, 16, 144, 1.0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white,
-                      ),
+              MaterialButton(
+                  minWidth: size.width * 1,
+                  height: size.width * 0.12,
+                  color: const Color.fromRGBO(199, 16, 144, 1.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
                     ),
-                    onPressed: () async {
-                      _auth
-                          .signInWithEmailAndPassword(
-                              email: _email, password: _password)
-                          .then((_) {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => const SwipeMessageProfile()));
-                      });
-                    }),
-              ),
+                  ),
+                  onPressed: () async {
+                    _auth
+                        .signInWithEmailAndPassword(
+                            email: _email, password: _password)
+                        .then((_) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const SwipeMessageProfile()));
+                    });
+                  }),
             ],
           )),
       Container(
@@ -189,12 +187,11 @@ class _SignInPageWidgetState extends State<SignInPage> {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                  child: RichText(
-                      text: TextSpan(
-                          style: TextStyle(color: Colors.white),
-                          text: 'Don\'t have an Account?',
-                          children: [
+              RichText(
+                  text: TextSpan(
+                      style: const TextStyle(color: Colors.white),
+                      text: 'Don\'t have an Account?',
+                      children: [
                     TextSpan(
                         recognizer: TapGestureRecognizer()
                           ..onTap = () {
@@ -204,9 +201,9 @@ class _SignInPageWidgetState extends State<SignInPage> {
                                     builder: (context) => SignUpPage()));
                           },
                         text: ' Sign Up Now',
-                        style:
-                            TextStyle(color: Color.fromRGBO(225, 16, 144, 1.0)))
-                  ])))
+                        style: const TextStyle(
+                            color: Color.fromRGBO(225, 16, 144, 1.0)))
+                  ]))
             ],
           ))
     ]));
