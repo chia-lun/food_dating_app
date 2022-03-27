edbbef2a5db3aa80a0c47d0800181ae1ecb7d96c
JiayingWu-work <70664833+JiayingWu-work@users.noreply.github.com>
2022-03-27 01:30:07 -0500

    enabled map with Macalester on the screen on main page

--- lib/screens/home/map_screen.dart
+++ lib/screens/home/map_screen.dart
@@ -1,3 +1,41 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  // Decimal Degrees format
+  final LatLng _center = const LatLng(44.9374234, -93.1692095);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition:
+                    CameraPosition(target: _center, zoom: 17.0),
+              ),
+            ),
+            Container(),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
